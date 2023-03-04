db7b92c6eda74fa118bbfb612db293351895c151
Ernest103 <ernest0399@icloud.com>
2023-03-04 12:41:26 -0500

    Boiler plate Project

---
+++ DateGen/DateGen/Models/DateInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DateGen.Models
+{
+    public class DateInfo
+    {
+        private Guid id { get;}
+        private DateTime date { get;}
+
+        public int? start { get; set; }
+        public int? end { get; set; }
+
+        public DateInfo(Guid id, DateTime date, int? start, int? end)
+        {
+            this.id = id;
+            this.date = date;
+            this.start = start;
+            this.end = end;
+        }
+    }
+}
